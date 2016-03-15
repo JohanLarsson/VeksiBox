2518a07cd4100d068c56246ffdaffc4f10dd0df8
Johan Larsson <johan_gavle1@hotmail.com>
2016-03-15 09:41:48 +0100

    Initial

---
+++ VeksiBox/BalloonControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace VeksiBox
+{
+    public class BalloonControl : ContentControl
+    {
+        static BalloonControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BalloonControl), new FrameworkPropertyMetadata(typeof(BalloonControl)));
+        }
+    }
+}
